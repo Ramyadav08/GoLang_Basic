199cf42d5ba04e89c8034acf17102ec6770b08cb
ramyadav08 <ramrekhayadav412@gmail.com>
2023-08-02 19:13:23 +0530

    latest update for the armstrong

--- linearsearch/main.go
+++ linearsearch/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 func main(){
 	items:=[]int{2,3,5,6,7,8,9,10,11}
-	fmt.Println(linearSearch(items,8))
+	fmt.Println(linearSearch(items,11))
 }
 
 func linearSearch(datalist []int ,key int) int{
